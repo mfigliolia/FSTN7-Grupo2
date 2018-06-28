3fb9bca61d245e8a6358b8aba06c808ab62f07c2
mfigliolia <mfigliolia23@yahoo.com.ar>
2018-06-28 18:24:49 -0300

    sprint3

---
+++ head.php
@@ -0,0 +1,7 @@
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/css?family=Play" rel="stylesheet">
+  <title>CELL.HOUSE</title>
+</head>
